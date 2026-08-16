baac912af7b950fb111b691fd49de68c159d9095
ovi-ab888 <oviabdulohab@gmail.com>
2026-08-16 15:43:15 +0600

    Update two_pieces_set.py

--- labels/two_pieces_set.py
+++ labels/two_pieces_set.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from engine.label_engine import fill_single_label, generate_multipage_pdf
 
 BASE_DIR = os.path.join(os.path.dirname(__file__), "..")
-TEMPLATE_PATH = os.path.join(BASE_DIR, "templates", "2-Pieces-Set.pdf")
+TEMPLATE_PATH = os.path.join(BASE_DIR, "templates", "two_pieces_set")
 CONFIG_PATH = os.path.join(BASE_DIR, "config", "pad_header_mapping.json")
 
 
